a1e1c4cf044515cb38c1c5548aadfd4362c302ae
Enol Fernandez <enol.fernandez@egi.eu>
2019-06-27 08:49:51 +0200

    Add refresh of credentials per VO

--- cloud-info/cloud-info-wrapper.sh
+++ cloud-info/cloud-info-wrapper.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-CLIENT_ID=$(cat $CHECKIN_CLIENT_ID_FILE)
-CLIENT_SECRET=$(cat $CHECKIN_CLIENT_SECRET_FILE)
-REFRESH_TOKEN=$(cat $CHECKIN_REFRESH_TOKEN_FILE)
-
-export OS_ACCESS_TOKEN=$(curl -s -X POST -u $CLIENT_ID:$CLIENT_SECRET \
-    -d "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&grant_type=refresh_token&refresh_token=$REFRESH_TOKEN&scope=openid" \
-    https://aai.egi.eu/oidc/token | jq -r ".access_token")
-
 # Other OS related parameter should be available as env variables
 cloud-info-provider-service --yaml-file $CLOUD_INFO_CONFIG \
                             --middleware openstack \
+                            --auth-refresher oidcvorefresh \
+                            --oidc-credentials-path $CHECKIN_SECRETS_PATH \
+                            --oidc-token-endpoint $CHECKIN_OIDC_TOKEN \
                             --format glue21
