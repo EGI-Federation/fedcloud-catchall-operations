2ee5ca7c0cd085954126c3fa6d0b3347cd35eb9b
Enol Fernández <enol.fernandez@egi.eu>
2019-09-12 15:45:07 +0100

    Support OCCI (ooi) endpoints (#13)
    * Take OCCI into account
    
    * Adapt scripts for OCCI
    
    Also do not try to create topic if not existing
    
    * Update sites
    
    Add OCCI endpoints

--- cloud-info/ams-wrapper.sh
+++ cloud-info/ams-wrapper.sh
@@ -5,8 +5,8 @@ set -e
 # This may fail if the OS_AUTH_URL is not the one registered in GOC
 GOCDB_ID=$(python -c "from __future__ import print_function; \
                       from cloud_info_provider.providers import gocdb; \
-                      print(gocdb.get_goc_info('$OS_AUTH_URL', \
-                                               'org.openstack.nova')['gocdb_id'], end='')")
+                      print(gocdb.get_goc_info('$GOCDB_URL', \
+                                               '$GOCDB_SERVICE_TYPE')['gocdb_id'], end='')")
 
 if test "x$AMS_TOKEN_FILE" != "x"; then
     AMS_TOKEN=$(cat $AMS_TOKEN_FILE)
@@ -14,9 +14,9 @@ fi
 
 AMS_TOPIC=SITE_${SITE_NAME}_ENDPOINT_${GOCDB_ID}
 
-# create TOPIC if not there
+# exit if TOPIC is not available.
 curl -f https://$AMS_HOST/v1/projects/$AMS_PROJECT/topics/$AMS_TOPIC\?key\=$AMS_TOKEN > /dev/null 2>&1 \
-    || curl -X PUT -f https://$AMS_HOST/v1/projects/$AMS_PROJECT/topics/$AMS_TOPIC\?key\=$AMS_TOKEN > /dev/null 2>&1
+    || (echo "Topic $AMS_TOPIC is not avaiable, aborting!"; false)
 
 cat > /etc/ams-clipw.settings << EOF
 [AMS]
---
+++ cloud-info/publisher.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+# OpenStack
+export GOCDB_URL=$OS_AUTH_URL
+export GOCDB_SERVICE_TYPE=org.openstack.nova
+export CLOUD_INFO_MIDDLEWARE=openstack
+
+ams-wrapper.sh
+
+if [ -n "$OCCI_ENDPOINT" ]; then
+    # OCCI
+    export GOCDB_URL=$OCCI_ENDPOINT
+    export GOCDB_SERVICE_TYPE=eu.egi.cloud.vm-management.occi
+    export CLOUD_INFO_MIDDLEWARE=ooi
+    ams-wrapper.sh
+fi
