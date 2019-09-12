b883e34a6996d1347c47f5a4782e14cfe2410380
Enol Fernandez <enol.fernandez@egi.eu>
2019-09-12 11:22:42 +0100

    Improve the build script

--- chartpress.sh
+++ chartpress.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-set -e
+set -euo pipefail
 
 # let's assume that we need to login if params are there
 if [ -n "$@" ]; then
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 fi
 
-if [ "x$TRAVIS_TAG" != "x" ]; then 
+if [ -n "$TRAVIS_TAG" ]; then
     CHARTPRESS_OPT="$CHARTPRESS_OPT --tag $TRAVIS_TAG"
 fi
-if [ "x$TRAVIS_COMMIT_RANGE" != "x" ]; then
+if [ -n "$TRAVIS_COMMIT_RANGE" ]; then
     CHARTPRESS_OPT="$CHARTPRESS_OPT --commit-range $TRAVIS_COMMIT_RANGE";
 fi
 
