7c63bc290796051efe604431c5ff5710db7a810f
Enol Fernandez <enol.fernandez@egi.eu>
2020-10-21 12:50:08 +0100

    Use cloud-info AMS publisher instead of ams-clipw
    This allows to move to python3 and also removes one dependency.

--- cloud-info/ams-wrapper.sh
+++ cloud-info/ams-wrapper.sh
@@ -5,8 +5,8 @@ set -e
 # This may fail if the OS_AUTH_URL is not the one registered in GOC
 GOCDB_ID=$(python -c "from __future__ import print_function; \
                       from cloud_info_provider.providers import gocdb; \
-                      print(gocdb.get_goc_info('$GOCDB_URL', \
-                                               '$GOCDB_SERVICE_TYPE')['gocdb_id'], end='')")
+		      print(gocdb.find_in_gocdb('$GOCDB_URL', \
+                                                '$GOCDB_SERVICE_TYPE')['gocdb_id'], end='')")
 
 if test "x$AMS_TOKEN_FILE" != "x"; then
     AMS_TOKEN=$(cat $AMS_TOKEN_FILE)
@@ -18,18 +18,20 @@ AMS_TOPIC=SITE_${SITE_NAME}_ENDPOINT_${GOCDB_ID}
 curl -f https://$AMS_HOST/v1/projects/$AMS_PROJECT/topics/$AMS_TOPIC\?key\=$AMS_TOKEN > /dev/null 2>&1 \
     || (echo "Topic $AMS_TOPIC is not avaiable, aborting!"; false)
 
-cat > /etc/ams-clipw.settings << EOF
-[AMS]
-ams_host: $AMS_HOST
-ams_project: $AMS_PROJECT
-ams_topic: $AMS_TOPIC
-msg_file_path:
-info_provider_path: /usr/local/bin/cloud-info-wrapper.sh 
+EXTRA_OPTS=""
 
-[AUTH]
-token: $AMS_TOKEN
-cert_path: $AMS_CERT_PATH
-key_path: $AMS_KEY_PATH
-EOF
+if [ -n "$DEBUG" ]; then
+    EXTRA_OPTS="$EXTRA_OPTS --debug"
+fi
 
-ams-clipw -c /etc/ams-clipw.settings
+# Other OS related parameter should be available as env variables
+cloud-info-provider-service --yaml-file $CLOUD_INFO_CONFIG \
+                            --middleware $CLOUD_INFO_MIDDLEWARE \
+                            --auth-refresher oidcvorefresh \
+                            --oidc-credentials-path $CHECKIN_SECRETS_PATH \
+                            --oidc-token-endpoint $CHECKIN_OIDC_TOKEN \
+                            --format glue21 \
+                            --publisher ams \
+                            --ams-token $AMS_TOKEN \
+                            --ams-topic $AMS_TOPIC \
+                            --ams-host $AMS_HOST $EXTRA_OPTS
