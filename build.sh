41655c96be91a625bca275412d6ee596d45f2bf7
Enol Fernandez <enol.fernandez@egi.eu>
2020-11-18 12:14:06 +0000

    Fix build

--- build.sh
+++ build.sh
@@ -4,9 +4,8 @@ set -euo pipefail
 
 set -x
 
-EXTRA_TAG=""
-if [ -n "$TRAVIS_TAG" ]; then
-    EXTRA_TAG="-t egifedcloud/ops-cloud-info:$TRAVIS_TAG"
-fi
+docker build -t egifedcloud/ops-cloud-info:latest $EXTRA_TAG cloud-info
 
-docker build -t egifedcloud/ops-cloud-info:latest "$EXTRA_TAG" cloud-info
+if [ -n "$TRAVIS_TAG" ]; then
+    docker build -t "egifedcloud/ops-cloud-info:$TRAVIS_TAG" cloud-info
+fi
