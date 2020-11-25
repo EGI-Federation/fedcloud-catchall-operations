14c688b161a050318a148fc42f1f4285fb6ecfa9
Enol Fernández <enol.fernandez@egi.eu>
2020-11-25 14:55:45 +0000

    Actions (#24)
    * Add inventory
    * Add github actions
    * Fix linting issues
    * Add a sample inventory
    * Sync with the repo template
    * Remove travis
    * Remove unused files
    * Validate schema
    * check schema of sites files
    * Use argument for version

--- cloud-info/ams-wrapper.sh
+++ cloud-info/ams-wrapper.sh
@@ -9,29 +9,24 @@ GOCDB_ID=$(python -c "from __future__ import print_function; \
                                                 '$GOCDB_SERVICE_TYPE')['gocdb_id'], end='')")
 
 if test "x$AMS_TOKEN_FILE" != "x"; then
-    AMS_TOKEN=$(cat $AMS_TOKEN_FILE)
+    AMS_TOKEN=$(cat "$AMS_TOKEN_FILE")
 fi
 
-AMS_TOPIC=SITE_${SITE_NAME}_ENDPOINT_${GOCDB_ID}
+AMS_TOPIC="SITE_${SITE_NAME}_ENDPOINT_${GOCDB_ID}"
 
 # exit if TOPIC is not available.
-curl -f https://$AMS_HOST/v1/projects/$AMS_PROJECT/topics/$AMS_TOPIC\?key\=$AMS_TOKEN > /dev/null 2>&1 \
+curl -f "https://$AMS_HOST/v1/projects/$AMS_PROJECT/topics/$AMS_TOPIC\?key\=$AMS_TOKEN" > /dev/null 2>&1 \
     || (echo "Topic $AMS_TOPIC is not avaiable, aborting!"; false)
 
-EXTRA_OPTS=""
-
-if [ -n "$DEBUG" ]; then
-    EXTRA_OPTS="$EXTRA_OPTS --debug"
-fi
 
 # Other OS related parameter should be available as env variables
-cloud-info-provider-service --yaml-file $CLOUD_INFO_CONFIG \
-                            --middleware $CLOUD_INFO_MIDDLEWARE \
+cloud-info-provider-service --yaml-file "$CLOUD_INFO_CONFIG" \
+                            --middleware "$CLOUD_INFO_MIDDLEWARE" \
                             --auth-refresher oidcvorefresh \
-                            --oidc-credentials-path $CHECKIN_SECRETS_PATH \
-                            --oidc-token-endpoint $CHECKIN_OIDC_TOKEN \
+                            --oidc-credentials-path "$CHECKIN_SECRETS_PATH" \
+                            --oidc-token-endpoint "$CHECKIN_OIDC_TOKEN" \
                             --format glue21 \
                             --publisher ams \
-                            --ams-token $AMS_TOKEN \
-                            --ams-topic $AMS_TOPIC \
-                            --ams-host $AMS_HOST $EXTRA_OPTS
+                            --ams-token "$AMS_TOKEN" \
+                            --ams-topic "$AMS_TOPIC" \
+                            --ams-host "$AMS_HOST"
