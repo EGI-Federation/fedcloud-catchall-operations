ffc0df738ee2e7b17bb2a7d377a48f300f44a9d0
Enol Fernandez <enol.fernandez@egi.eu>
2021-06-17 14:12:39 +0100

    Improve script

--- validate.sh
+++ validate.sh
@@ -5,18 +5,18 @@ goc_method="https://goc.egi.eu/gocdbpi/public/?method=get_service_endpoint"
 exit_value=0
 
 for f in sites/*.yaml; do
-     goc_site=$(grep "^gocdb:" $f | cut -f2 -d":" | tr -d [:space:])
-     endpoint=$(grep "^endpoint:" $f | cut -f2- -d":" | tr -d [:space:])
+     goc_site=$(grep "^gocdb:" "$f" | cut -f2 -d":" | tr -d "[:space:]")
+     endpoint=$(grep "^endpoint:" "$f" | cut -f2- -d":" | tr -d "[:space:]")
      echo "Searching for endpoint $endpoint in $goc_site site"
-     curl -s "$goc_method&sitename=$goc_site&service_type=org.openstack.nova" > /tmp/site-$goc_site.xml
-     grep -q "<SITENAME>$goc_site</SITENAME>" /tmp/site-$goc_site.xml 
-     if [ $? -ne 0 ]; then
-	echo -e "\033[0;31m[ERROR] Site $goc_site not found in GOC\033[0m"
+     curl -s "$goc_method&sitename=$goc_site&service_type=org.openstack.nova" > "/tmp/site-$goc_site.xml"
+     if ! grep -q "<SITENAME>$goc_site</SITENAME>" "/tmp/site-$goc_site.xml";
+     then
+	echo "\033[0;31m[ERROR] Site $goc_site not found in GOC\033[0m"
 	exit_value=1
 	continue
      fi
-     grep -q "<URL>$endpoint</URL>" /tmp/site-$goc_site.xml 
-     if [ $? -ne 0 ]; then
+     if ! grep -q "<URL>$endpoint</URL>" "/tmp/site-$goc_site.xml";
+     then
 	echo "\033[0;31m[ERROR] URL $endpoint for $goc_site not found in GOC\033[0m"
 	exit_value=1
      else
