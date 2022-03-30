ba76c00110ad53d3d083830384b4fff5888bd570
Enol Fernandez <enol.fernandez@egi.eu>
2022-03-30 12:36:04 +0100

    Linter fixes

--- cloudkeeper-core/entrypoint.sh
+++ cloudkeeper-core/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ "x$DEBUG" = "x1" ] ; then
+if [ "$DEBUG" = "1" ] ; then
     set -x
 fi
 
 EXTRA_OPTS=""
 
-if [ "x$BACKEND_PORT_50051_TCP_ADDR" != "x" ]; then
+if [ "$BACKEND_PORT_50051_TCP_ADDR" != "" ]; then
     EXTRA_OPTS="$EXTRA_OPTS --backend-endpoint=$BACKEND_PORT_50051_TCP_ADDR:$BACKEND_PORT_50051_TCP_PORT"
 fi
 
-exec $@ $EXTRA_OPTS
+exec "$@" "$EXTRA_OPTS"
