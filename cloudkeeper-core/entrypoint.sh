e05af25da018908517e8da5bd9df9d6a5ff1aec1
Enol Fernandez <enol.fernandez@egi.eu>
2022-03-31 13:16:18 +0100

    Avoid spaces being considered here

--- cloudkeeper-core/entrypoint.sh
+++ cloudkeeper-core/entrypoint.sh
@@ -10,4 +10,4 @@ if [ "$BACKEND_PORT_50051_TCP_ADDR" != "" ]; then
     EXTRA_OPTS="$EXTRA_OPTS --backend-endpoint=$BACKEND_PORT_50051_TCP_ADDR:$BACKEND_PORT_50051_TCP_PORT"
 fi
 
-exec "$@" "$EXTRA_OPTS"
+exec "$@" $EXTRA_OPTS
