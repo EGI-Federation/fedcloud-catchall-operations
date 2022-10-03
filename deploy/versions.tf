a9779cb875ab7984c59e9d610fb7a638f8f9eedc
Enol Fernández <enol.fernandez@egi.eu>
2022-10-03 09:40:55 +0100

    Tell gitleak to ignore APT key (#225)
    * Tell gitleak to ignore APT key
    
    * Make linter happy
    
    * Move the version requirement to the right place
    
    * Terraform format

--- deploy/versions.tf
+++ deploy/versions.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     openstack = {
-      source = "terraform-provider-openstack/openstack"
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.48"
     }
   }
   required_version = ">= 0.13"
