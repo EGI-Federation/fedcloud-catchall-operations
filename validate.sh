e8582efc872e0238af5a3a41498558956e68fab8
Enol Fernandez <enol.fernandez@egi.eu>
2023-01-11 09:57:35 +0000

    Make linter happy

--- validate.sh
+++ validate.sh
@@ -6,26 +6,30 @@ exit_value=0
 
 # Get all VOs names
 VO_LIST=$(mktemp)
-curl "http://cclavoisier01.in2p3.fr:8080/lavoisier/VoList?accept=json" | jq -r ".data[].name" > "$VO_LIST"
+curl -s "http://cclavoisier01.in2p3.fr:8080/lavoisier/VoList?accept=json" \
+    | jq -r ".data[].name" > "$VO_LIST"
 
 for f in sites/*.yaml
 do
     goc_site=$(grep "^gocdb:" "$f" | cut -f2 -d":" | tr -d "[:space:]")
     endpoint=$(grep "^endpoint:" "$f" | cut -f2- -d":" | tr -d "[:space:]")
-    echo "Searching for endpoint $endpoint in $goc_site site ($f)"
-    curl -s "$goc_method&sitename=$goc_site&service_type=org.openstack.nova" > "/tmp/site-$goc_site.xml"
+    printf "Searching for endpoint %s in %s site (%s)\n" \
+        "$endpoint"  "$goc_site" "$f"
+    curl -s "$goc_method&sitename=$goc_site&service_type=org.openstack.nova" \
+        > "/tmp/site-$goc_site.xml"
     if ! grep -q "<SITENAME>$goc_site</SITENAME>" "/tmp/site-$goc_site.xml"
     then
-        echo "\033[0;31m[ERROR] Site $goc_site not found in GOC\033[0m"
+        printf "\033[0;31m[ERROR] Site %s not found in GOC\033[0m\n" "$goc_site"
         exit_value=1
         continue
     fi
     if ! grep -q "<URL>$endpoint</URL>" "/tmp/site-$goc_site.xml"
     then
-        echo "\033[0;31m[ERROR] URL $endpoint for $goc_site not found in GOC\033[0m"
+        printf "\033[0;31m[ERROR] URL %s for %s not found in GOC\033[0m\n" \
+            "$endpoint" "$goc_site"
         exit_value=1
     else
-        echo "\033[0;32m[OK]\033[0m"
+        printf "\033[0;32m[OK]\033[0m\n"
      fi
     # check if all VOs configured do exist
     # Try to use FQAN
@@ -37,7 +41,8 @@ do
     do
         if ! grep -q "^$vo\$" "$VO_LIST"
         then
-            echo "\033[0;31m[ERROR] VO $vo not found in ops portal\033[0m"
+            printf "\033[0;31m[ERROR] VO %s not found in ops portal\033[0m\n" \
+                "$vo"
             exit_value=1
         fi
     done
@@ -46,4 +51,3 @@ done
 rm "$VO_LIST"
 
 exit $exit_value
-
