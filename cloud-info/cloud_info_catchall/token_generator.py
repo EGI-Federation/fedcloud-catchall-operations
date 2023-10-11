b63edf0ea9c95fad80e004f6c14807c894b2f7a1
Enol Fernandez <enol.fernandez@egi.eu>
2023-10-11 14:39:23 +0100

    Improve code of generator

--- cloud-info/cloud_info_catchall/token_generator.py
+++ cloud-info/cloud_info_catchall/token_generator.py
@@ -2,9 +2,9 @@
 
 Takes its own configuration from env variables:
 CHECKIN_SECRETS_FILE: yaml file with the check-in secrets to get access tokens
-CHECKIN_OIDC_TOKEN: URL for token refreshal
 CHECKIN_SCOPES: Scopes to request in the access token
-ACCESS_TOKEN_DIR: Directory with the current access tokens / where to dump the new ones
+CHECKIN_OIDC_URL: Discovery URL for Check-in
+ACCESS_TOKEN_SECRETS_FILE: File where to dump the new access tokens if needed 
 ACCESS_TOKEN_TTL: Minimum TTL for the access token
 """
 
@@ -42,12 +42,8 @@ def get_access_token(token_url, scopes, secret):
     return r.json()["access_token"]
 
 
-def valid_token(token_file, oidc_config, min_time):
-    try:
-        with open(token_file, "r") as f:
-            access_token = f.read()
-    except FileNotFoundError as e:
-        logging.info(f"Previous access token not found: {e}")
+def valid_token(token, oidc_config, min_time):
+    if not token:
         return False
     jwks_config = requests.get(oidc_config["jwks_uri"]).json()
     # See https://stackoverflow.com/a/68891371
@@ -56,12 +52,10 @@ def valid_token(token_file, oidc_config, min_time):
         kid = jwk["kid"]
         public_keys[kid] = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
     try:
-        headers = jwt.get_unverified_header(access_token)
+        headers = jwt.get_unverified_header(token)
         kid = headers["kid"]
         key = public_keys[kid]
-        payload = jwt.decode(
-            access_token, key=public_keys[kid], algorithms=[headers["alg"]]
-        )
+        payload = jwt.decode(token, key=public_keys[kid], algorithms=[headers["alg"]])
         # this comes from JWT documentation
         # https://pyjwt.readthedocs.io/en/stable/usage.html#expiration-time-claim-exp
         now = calendar.timegm(datetime.now(tz=timezone.utc).utctimetuple())
@@ -71,34 +65,38 @@ def valid_token(token_file, oidc_config, min_time):
         return False
 
 
-def generate_tokens(oidc_config, scopes, access_token_dir, token_ttl, secrets):
+def generate_tokens(oidc_config, scopes, tokens, token_ttl, secrets):
+    new_tokens = {}
     for s in secrets:
         # not our thing
         if not isinstance(secrets[s], dict):
             continue
-        token_file = os.path.join(access_token_dir, s)
-        if not valid_token(token_file, oidc_config, token_ttl):
+        token = tokens.get(s, {}).get("access_token", None)
+        if not valid_token(token, oidc_config, token_ttl):
             logging.info("Token needs refreshing")
-            with open(token_file, "w+") as f:
-                f.write(
-                    get_access_token(oidc_config["token_endpoint"], scopes, secrets[s])
-                )
+            token = get_access_token(oidc_config["token_endpoint"], scopes, secrets[s])
         else:
             logging.info("Token is still valid, not refreshing")
+        new_tokens[s] = {"access_token": token}
+    return new_tokens
 
 
 def main():
-    logging.basicConfig()
+    logging.basicConfig(level=logging.DEBUG)
     # get config from env
     checkin_secrets_file = os.environ["CHECKIN_SECRETS_FILE"]
     oidc_config_url = os.environ.get("CHECKIN_OIDC_URL", CHECKIN_OIDC_URL)
     oidc_config = requests.get(oidc_config_url).json()
     scopes = os.environ.get("CHECKIN_SCOPES", CHECKIN_SCOPES)
-    access_token_dir = os.environ["ACCESS_TOKEN_DIR"]
-    os.makedirs(access_token_dir, exist_ok=True)
+    access_token_file = os.environ["ACCESS_TOKEN_FILE"]
     token_ttl = int(os.environ.get("ACCESS_TOKEN_TTL", ACCESS_TOKEN_TTL))
     secrets = read_secrets(checkin_secrets_file)
-    generate_tokens(oidc_config, scopes, access_token_dir, token_ttl, secrets)
+    tokens = {}
+    if os.path.exists(access_token_file):
+        tokens.update(read_secrets(access_token_file))
+    new_tokens = generate_tokens(oidc_config, scopes, tokens, token_ttl, secrets)
+    with open(access_token_file, "w+") as f:
+        f.write(yaml.dump(new_tokens))
 
 
 if __name__ == "__main__":
