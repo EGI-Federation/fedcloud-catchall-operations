ad691a2321a2b6f515173d8b7943e3c9e4e8b2f0
Enol Fernandez <enol.fernandez@egi.eu>
2024-07-10 08:52:08 +0100

    Add volume

--- image-sync/deploy/main.tf
+++ image-sync/deploy/main.tf
@@ -1,3 +1,8 @@
+resource "openstack_blockstorage_volume_v3" "image-cache" {
+  name = "image-cache"
+  size = 200
+}
+
 resource "openstack_compute_instance_v2" "image-sync" {
   name            = "image-sync"
   image_id        = var.image_id
@@ -9,6 +14,13 @@ resource "openstack_compute_instance_v2" "image-sync" {
   }
 }
 
+resource "openstack_compute_volume_attach_v2" "attached" {
+  instance_id = openstack_compute_instance_v2.image-sync.id
+  volume_id   = openstack_blockstorage_volume_v3.image-cache.id
+}
+
+
+
 output "instance-id" {
-  value = openstack_compute_instance_v2.cloud-info.id
+  value = openstack_compute_instance_v2.image-sync.id
 }
