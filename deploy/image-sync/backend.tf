e309efa1e6410207c0885aa64574632507a2a676
Enol Fernandez <enol.fernandez@egi.eu>
2024-07-12 12:17:36 +0100

    Fine tune deployment

--- deploy/image-sync/backend.tf
+++ deploy/image-sync/backend.tf
@@ -1,10 +1,10 @@
-## This is where the info about the deployment is to be stored
-#terraform {
-#  backend "swift" {
-#    container = "terraform"
-#    cloud     = "backend"
-#  }
-#}
+# This is where the info about the deployment is to be stored
+terraform {
+  backend "swift" {
+    container = "terraform-image-sync"
+    cloud     = "backend"
+  }
+}
 
 # The provider where the deployment is actually performed
 provider "openstack" {
