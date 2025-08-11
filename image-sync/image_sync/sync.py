9be9e6a36765cec5bb879d64c2b20c87a3bcedd6
Enol Fernandez <enol.fernandez@egi.eu>
2025-08-11 14:01:26 +0100

    feat(image-sync): Sync also other VOs
    Fetch from harbor the list of available projects and sync them with the
    matching VOs at the sites.

--- image-sync/image_sync/sync.py
+++ image-sync/image_sync/sync.py
@@ -1,3 +1,10 @@
+"""
+Image synchronisation tool
+
+Creates the proper configuration files for atrope and runs it to get images
+synced in the EGI Fedcloud
+"""
+
 import glob
 import logging
 import os
@@ -10,19 +17,22 @@ import httpx
 import yaml
 from oslo_config import cfg
 
+
 # Configuraion
 CONF = cfg.CONF
 CONF.register_opts(
     [
         cfg.StrOpt("site_config_dir", default="."),
         cfg.StrOpt("cloud_info_url", default="https://is.cloud.egi.eu"),
-        cfg.StrOpt("registry_api_url", default="https://registry.egi.eu/api/v2.0"),
+        cfg.StrOpt("registry_base_url", default="https://registry.egi.eu"),
         cfg.StrOpt("registry_host", default="registry.egi.eu"),
         cfg.StrOpt("registry_project", default="egi_vm_images"),
         cfg.ListOpt("formats", default=[]),
-        cfg.StrOpt("appdb_token"),
         cfg.StrOpt("registry_user"),
         cfg.StrOpt("registry_password"),
+        cfg.StrOpt(
+            "registry_project_map", default="/image-sync/registry-projects.yaml"
+        ),
     ],
     group="sync",
 )
@@ -41,6 +51,36 @@ CONF.register_opts(
     ],
     group="checkin",
 )
+
+
+def fetch_harbor_projects():
+    auth = httpx.BasicAuth(
+        username=CONF.sync.registry_user, password=CONF.sync.registry_password
+    )
+    client = httpx.Client(auth=auth)
+
+    projects = []
+    page = 1
+    next_url = "/api/v2.0/projects"
+    params = dict(page=page, page_size=10)
+
+    while next_url:
+        url = f"{CONF.sync.registry_base_url}{next_url}"
+        logging.debug(f"Fetching page {page} from {url}")
+        r = client.get(url, params=params)
+        r.raise_for_status()
+        data = r.json()
+        if not data:
+            break
+        projects.extend(data)
+        next_url = r.links.get("next", {}).get("url", None)
+        print(next_url)
+        params = {}
+        page += 1
+
+    # FIXME: we may want to include some metadata in harbor instead of just
+    #        matching names. For now this should work
+    return [p.get("name") for p in projects]
 
 
 def fetch_site_info_cloud_info():
@@ -122,25 +162,35 @@ image_sources = {sources_file}
     )
 
 
-def dump_sources_config(vo_list):
+def dump_sources_config(site_vo_list, harbor_projects):
     harbor = {
-        CONF.sync.registry_project: {
+        "harbor": {
             "enabled": True,
-            "vos": vo_list,
-            "type": "harbor",
-            "api_url": CONF.sync.registry_api_url,
             "prefix": "registry.egi.eu ",
-            "project": CONF.sync.registry_project,
-            "registry_host": CONF.sync.registry_host,
+            "api_url": f"{CONF.sync.registry_base_url}/api/v2.0",
             "auth_user": CONF.sync.registry_user,
             "auth_password": CONF.sync.registry_password,
+            "registry_host": CONF.sync.registry_host,
             "tag_pattern": "^[^-]*$",
-        }
+        },
+        # All vos uses the default registry project
+        CONF.sync.registry_project: {
+            "vos": site_vo_list,
+            "type": "harbor",
+            "project": CONF.sync.registry_project,
+        },
     }
+    # specific VOs use their own project
+    for project in filter(lambda x: x in site_vo_list, harbor_projects):
+        harbor[project] = {
+            "vos": project,
+            "type": "harbor",
+            "project": project,
+        }
     return yaml.dump(harbor)
 
 
-def do_sync(sites_config):
+def do_sync(sites_config, harbor_projects):
     sites_info = fetch_site_info()
     for site in sites_info:
         site_name = site["site"]["name"]
@@ -157,14 +207,14 @@ def do_sync(sites_config):
         with tempfile.TemporaryDirectory() as tmpdirname:
             sources_file = os.path.join(tmpdirname, "sources.yaml")
             vo_map_file = os.path.join(tmpdirname, "vo-map.yaml")
-            vo_list = list(site["shares"].keys())
+            site_vo_list = list(site["shares"].keys())
             ops_project_id = site["shares"]["ops"]["project_id"]
             with open(os.path.join(tmpdirname, "atrope.conf"), "w+") as f:
                 f.write(
                     dump_atrope_config(site, ops_project_id, sources_file, vo_map_file)
                 )
             with open(sources_file, "w+") as f:
-                f.write(dump_sources_config(vo_list))
+                f.write(dump_sources_config(site_vo_list, harbor_projects))
             with open(vo_map_file, "w+") as f:
                 f.write(yaml.dump(site["shares"]))
             cmd = [
@@ -189,7 +239,7 @@ def load_sites():
 def main():
     CONF(sys.argv[1:])
     logging.basicConfig(level=logging.DEBUG)
-    do_sync(load_sites())
+    do_sync(load_sites(), fetch_harbor_projects())
 
 
 if __name__ == "__main__":
