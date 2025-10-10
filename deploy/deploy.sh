6fe63d8fa9fd057abcfddda4f3d343b58afe511f
Enol Fernández <enol.fernandez@egi.eu>
2025-10-10 07:22:03 +0100

    Apply suggestion from @sebastian-luna-valero
    Co-authored-by: Sebastian Luna-Valero <sebastian-luna-valero@users.noreply.github.com>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -35,7 +35,7 @@ CLIENT_ID=$(yq -r '.checkin.client_id' <secrets.yaml)
 CLIENT_SECRET=$(yq -r '.checkin.client_secret' <secrets.yaml)
 SCOPE="openid%20email%20profile%20voperson_id"
 SCOPE="$SCOPE%20eduperson_entitlement:urn:mace:egi.eu:group:cloud.egi.eu:role=vm_operator#aai.egi.eu"
-SCOPE="$SCOPE%20eduperson_entitlement:urn:mace:egi.eu:group:cloud.egi.eu:role=vm_operator#aai.egi.eu"
+SCOPE="$SCOPE%20eduperson_entitlement:urn:mace:egi.eu:group:cloud.egi.eu:role=member#aai.egi.eu"
 SCOPE="$SCOPE%20entitlements:urn:mace:egi.eu:group:cloud.egi.eu:role=member#aai.egi.eu"
 SCOPE="$SCOPE%20entitlements:urn:mace:egi.eu:group:cloud.egi.eu:role=member#aai.egi.eu"
 ACCESS_TOKEN=$(curl --request POST "https://aai.egi.eu/auth/realms/egi/protocol/openid-connect/token" \
