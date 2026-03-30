3e6414811bab363b6c144e65520e035eb4bb1dd5
Enol Fernández <enol.fernandez@egi.eu>
2026-03-30 11:55:03 +0100

    Provider `project_id` in the mapping
    atrope relies on this one to be available (and it's easier to add it here than to change atrope ;))

--- src/fedcloud_catchall/image_sync.py
+++ src/fedcloud_catchall/image_sync.py
@@ -127,6 +127,15 @@ def dump_sources_config(site_vo_list, harbor_projects):
     return yaml.dump(harbor)
 
 
+def dump_vo_map(site):
+    shares = site.get("shares")
+    for name, vo in shares.items():
+        project_id = vo.get("id")
+        if project_id:
+            vo["project_id"] = project_id
+    return yaml.dump(shares)
+
+
 def do_sync(sites_config, harbor_projects):
     sites_info = fetch_site_info()
     for site in sites_info:
